f4eee39fb8d5173c860b0930f5db5d4d559a0386
wireboy <wireboy@163.com>
2019-02-18 22:27:21 +0800

    解决断开后无法再次连接的问题

--- src/P2PServiceHome/P2PService.cs
+++ src/P2PServiceHome/P2PService.cs
@@ -21,6 +21,9 @@ namespace P2PServiceHome
         DateTime lastReceiveTime = DateTime.Now;
 
         TcpClient heartClient = null;
+        Guid inGuid = Guid.NewGuid();
+        Guid outGuid = Guid.NewGuid();
+        TaskFactory taskFactory = new TaskFactory();
         public P2PService()
         {
         }
@@ -53,58 +56,63 @@ namespace P2PServiceHome
                         //发送心跳包
                         heartClient.Client.Send(new byte[] { 0 });
                     }
-                }catch(Exception ex)
+                }
+                catch (Exception ex)
                 {
                     heartClient = null;
                     outClient = null;
-                    Console.WriteLine("服务器连接失败，稍后将重连... {0}", ex);
+                    //Console.WriteLine("服务器连接失败，稍后将重连... {0}", ex);
                 }
                 try
                 {
                     if (outClient == null || !outClient.Connected)
                     {
-                        Console.WriteLine("正在连接服务器... {0}:{1}", service_IpAddress, service_Port);
+                        //Console.WriteLine("正在连接服务器... {0}:{1}", service_IpAddress, service_Port);
                         outClient = new TcpClient(service_IpAddress, service_Port);
-                        Console.WriteLine("服务器成功连接");
+                        //Console.WriteLine("服务器成功连接");
 
                         NetworkStream ssOut = outClient.GetStream();
                         List<byte> sMsg = Encoding.ASCII.GetBytes(ServerName).ToList();
                         sMsg.Insert(0, 55);
                         ssOut.Write(sMsg.ToArray(), 0, sMsg.ToArray().Length);
-                        new TaskFactory().StartNew(() => { clientReceive(outClient); });
+                        Guid guid = Guid.NewGuid();
+                        outGuid = guid;
+                        taskFactory.StartNew(() => { clientReceive(outClient, guid); });
                     }
                 }
                 catch (Exception ex)
                 {
                     outClient = null;
-                    Console.WriteLine("服务器连接失败，稍后将重连... {0}", ex);
+                    //Console.WriteLine("服务器连接失败，稍后将重连... {0}", ex);
                 }
                 try
                 {
                     if (inClient == null || !inClient.Connected)
                     {
-                        Console.WriteLine("正在连接本地远程桌面服务... 127.0.0.1:3389");
+                        //Console.WriteLine("正在连接本地远程桌面服务... 127.0.0.1:3389");
                         inClient = new TcpClient();
                         inClient.Connect(IPAddress.Parse("127.0.0.1"), 3389);
-                        Console.WriteLine("本地远程桌面服务连接成功");
-                        new TaskFactory().StartNew(() => { clientReceive(inClient); });
+                        //Console.WriteLine("本地远程桌面服务连接成功");
+                        Guid guid = Guid.NewGuid();
+                        inGuid = guid;
+                        taskFactory.StartNew(() => { clientReceive(inClient, guid); });
                     }
                 }
                 catch (Exception ex)
                 {
                     inClient = null;
-                    Console.WriteLine("本地远程桌面服务连接失败，稍后将重连... {0}", ex.Message);
+                    //Console.WriteLine("本地远程桌面服务连接失败，稍后将重连... {0}", ex.Message);
                 }
                 Thread.Sleep(2000);
             } while (true);
 
         }
         Task checkDeskConnectedTask = null;
-        private void clientReceive(TcpClient client)
+        private void clientReceive(TcpClient client, Guid curGuid)
         {
             bool isLocalClient = client == inClient;
 
-            while (true && client != null)
+            while (client != null && curGuid == (isLocalClient ? inGuid : outGuid))
             {
                 byte[] recBytes = new byte[10240];
                 int count = 0;
@@ -114,20 +122,22 @@ namespace P2PServiceHome
                 }
                 catch (Exception ex)
                 {
-                    if(isLocalClient)
+                    TcpClient tempClient = inClient;
+                    if (isLocalClient)
                     {
-                        TcpClient tempClient = inClient;
                         inClient = null;
                         tempClient.Close();
                     }
                     else
                     {
-                        TcpClient tempClient = outClient;
                         outClient = null;
+                    }
+                    try
+                    {
                         tempClient.Close();
                     }
-
-                    Console.WriteLine("接收数据异常：{0}", ex);
+                    catch { }
+                    //Console.WriteLine("接收数据异常：{0}", ex);
                     break;
                 }
                 if (count > 0)
@@ -135,14 +145,14 @@ namespace P2PServiceHome
                     if (!isLocalClient)
                     {
                         if (checkDeskConnectedTask == null)
-                            checkDeskConnectedTask = new TaskFactory().StartNew(() => { CheckDeskConnected(); });
+                            checkDeskConnectedTask = taskFactory.StartNew(() => { CheckDeskConnected(inGuid); });
                     }
                     else
                     {
                         //远程桌面发送了数据
                         lastReceiveTime = DateTime.Now;
                     }
-                    //Console.WriteLine("从{0}接收到数据,长度：{1}", client.Client.RemoteEndPoint,count);
+                    ////Console.WriteLine("从{0}接收到数据,长度：{1}", client.Client.RemoteEndPoint,count);
                     TcpClient toClient = isLocalClient ? outClient : inClient;
                     if (toClient != null)
                     {
@@ -154,46 +164,51 @@ namespace P2PServiceHome
                         }
                         catch (Exception ex)
                         {
+                            TcpClient tempClient = outClient;
                             if (isLocalClient)
                             {
-                                Console.WriteLine("向服务器发送数据失败！{0}",ex);
-                                TcpClient tempClient = outClient;
+                                //Console.WriteLine("向服务器发送数据失败！{0}", ex);
                                 outClient = null;
-                                tempClient.Close();
                             }
                             else
                             {
-                                Console.WriteLine("向本地远程桌面服务发送数据失败！{0}", ex);
-                                TcpClient tempClient = inClient;
+                                //Console.WriteLine("向本地远程桌面服务发送数据失败！{0}", ex);
                                 inClient = null;
+                            }
+                            try
+                            {
                                 tempClient.Close();
                             }
+                            catch { }
                         }
                     }
                 }
-
                 client = isLocalClient ? inClient : outClient;
             }
         }
 
-        private void CheckDeskConnected()
+        private void CheckDeskConnected(Guid curGuid)
         {
-            Console.WriteLine("启动本地远程桌面服务通讯守护线程！");
+            //Console.WriteLine("启动本地远程桌面服务通讯守护线程！");
             lastReceiveTime = DateTime.Now;
-            while (true && inClient != null)
+            TcpClient tempClient = inClient;
+            while (curGuid == inGuid && inClient != null)
             {
                 if ((DateTime.Now - lastReceiveTime).Milliseconds > 3000)
                 {
-                    Console.WriteLine("本地远程桌面服务连接超时！");
+                    //Console.WriteLine("本地远程桌面服务连接超时！");
                     lastReceiveTime = DateTime.Now;
-                    TcpClient tempClient = inClient;
                     inClient = null;
-                    tempClient.Close();
                     break;
                 }
-                Thread.Sleep(1000);
+                Thread.Sleep(500);
             }
-            Console.WriteLine("本地远程桌面服务守护线程退出！");
+            try
+            {
+                tempClient.Close();
+            }
+            catch { }
+            //Console.WriteLine("本地远程桌面服务守护线程退出！");
             checkDeskConnectedTask = null;
         }
     }
