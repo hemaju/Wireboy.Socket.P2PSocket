c7f6e97af6e82aee060b45f9e33d20aaaf987bdc
wireboy <wireboy@163.com>
2019-04-11 18:06:17 +0800

    新增自更新脚本

---
+++ utils/AutoUpdate.bat
@@ -0,0 +1,5 @@
+taskkill /im Wireboy.Socket.P2PClient.exe
+ping 127.0.0.1
+del /F /S /Q Wireboy.Socket.P2PClient.exe
+ren Wireboy.Socket.P2PClient2.exe Wireboy.Socket.P2PClient.exe
+Wireboy.Socket.P2PClient.exe
