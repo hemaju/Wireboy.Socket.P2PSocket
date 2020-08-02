d49ac67b04024a65f980e3d3f4d6943862052147
wireboy <wireboy@163.com>
2020-08-02 23:53:20 +0800

    移除Demo项目

---
+++ src/P2PSocekt.Core/Models/PropertyStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace P2PSocket.Core.Models
+{
+    public class PropertyStore : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            RaisePropertyChanged(PropertyName);
+        }
+
+        protected void RaiseAllChanged()
+        {
+            RaisePropertyChanged("");
+        }
+
+        protected bool Set<T>(ref T Field, T Value, [CallerMemberName] string PropertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(Field, Value))
+                return false;
+            Field = Value;
+            RaisePropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
