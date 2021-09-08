5ff3f6e5e78b93dd7ceec7134acb4e3663f64b83
wireboy <wireboy@163.com>
2021-09-08 18:39:42 +0800

    【4.0.0版本】1.涅槃重生

--- utils/AutoUpdate.bat
+++
@@ -1,5 +0,0 @@
-taskkill /im Wireboy.Socket.P2PClient.exe
-ping 127.0.0.1
-del /F /S /Q Wireboy.Socket.P2PClient.exe
-ren Wireboy.Socket.P2PClient2.exe Wireboy.Socket.P2PClient.exe
-Wireboy.Socket.P2PClient.exe
